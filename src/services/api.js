8db42362b8ecd3ce3843ebdf764e4ae42bb6e679
Welder Rodrigues <welder_15@hotmail.com>
2022-08-09 22:04:41 -0300

    adicionado URL da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.2.107:3333",
+  baseURL: "https://codigo-barra-api.herokuapp.com",
+  //baseURL: "http://192.168.2.107:3333",
 });
 
 export default api;
